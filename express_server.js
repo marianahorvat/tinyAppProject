0e4151c358b8b18b5a93b253fd131d3db2372f39
Mariana <mariana.horvat.qa@gmail.com>
2019-04-11 17:15:48 +0000

    Implemented Cookies in Express

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 var express = require("express");
+var cookieParser = require('cookie-parser');
 var app = express();
+
 var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");   //Asking the app to use EJS as its templating engine
+app.use(cookieParser());         //Asking the app to use cookieParser parameter 
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,8 +15,9 @@ var urlDatabase = {
 const bodyParser = require("body-parser");   //This should be declared before all of the routes
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/urls/new", (req, res) => {                     //    http://localhost:8080/urls/new
-  res.render("urls_new");                               //GET Route to Show the Form to the User
+app.get("/urls/new", (req, res) => { 
+  let templateVars = {username: req.cookies["username"]}                    //    http://localhost:8080/urls/new
+  res.render("urls_new", templateVars);                               //GET Route to Show the Form to the User
 });        //s/b before app.get("/urls/:id", ...) any calls to /urls/new will be handled by app.get("/urls/:id", ...) 
 
 app.get("/", (req, res) => {
@@ -29,13 +33,13 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {                         //http://localhost:8080/urls
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {  //   http://localhost:8080/urls/b2xVn2
   var shortUrlName = req.params.shortURL;
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortUrlName]};   /* What goes here? */ 
+  let templateVars = {username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[shortUrlName]};   /* What goes here? */ 
   res.render("urls_show", templateVars);
 });
 
@@ -49,6 +53,16 @@ app.post("/urls", (req, res) => {
   let randShortURL = generateRandomString();
   urlDatabase[randShortURL] = req.body.longURL;
   res.redirect(`/urls/${randShortURL}`);
+});
+
+app.post("/logout", (req,res) => {                     //Logout
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
+app.post("/login", (req,res) => {                      //http://localhost:8080/urls/login
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {      //   Delete shortURL http://localhost:8080/urls/
